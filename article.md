2632adc4b3d7f825456a5ebe0c9a776320656e4f
devifeoma <judithcynthia1@gmail.com>
2022-01-08 18:37:24 +0100

    Add Leaflet map

---
+++ article.md
@@ -0,0 +1,46 @@
+# Building an IP Address Tracker with react.js, Leaflet API and IP Geolocation API.
+
+![alt text](./src/img/map.jpg)
+
+## Introduction
+In this tutorial, we will learn how to build an IP Address Tracker that allows you to know your client location using Leaflet and IP Geolocation API. 
+
+## Prerequisite
+- Node installed in your local machine
+- Basic knowledge of React Js
+- Create an account on both Leaflet and  IP Geolocation API.
+
+## What is an IP Address?
+According to Wikipedia, IP Address is a numerical label such as 192.0.2.1 that is connected to a computer network that uses the Internet Protocol for communication. In other words, an IP Address is a unique identifier and can be used to trace ones online activities.
+
+## Install and Setup React.Js
+Let's setup our environment so we can get up and running with our application.
+
+```sh
+npm install -g react-cli react
+```
+
+To create a new react project we can use the command below to start our own project 
+
+```sh
+npx create-react-app ip-address-tracker
+```
+
+You will be asked to select some options while creating your project. You can either go by the default setting or select the option manually and then navigate to the project directory
+
+```sh
+cd ip-address-tracker
+```
+
+Finally serve your react js development server with
+
+```
+npm start
+```
+
+## Building the actual application
+
+
+
+
+## Conclusion
--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef} from 'react';
 import './App.css';
+import L from "leaflet"
+import "./../node_modules/leaflet/dist/leaflet.css"
+import {FaAngleRight} from "react-icons/fa"
+
+const accessToken = "pk.eyJ1IjoianVkaXRoY3ludGhpYTEiLCJhIjoiY2t5NXJjeW9kMGpvcDJwczFwbXdhcDBlaCJ9.t1hVDGZjeiyXRMyyJQ4_LA"
 
 function App() {
+  const mapRef = useRef()
+  useEffect(()=>{
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/1/1/0?access_token=${accessToken}`, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: accessToken
+    }).addTo(map);
+  },[])
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div  className="input-div">
+        <h1 className="IP-Address">IP Address Tracker</h1>
+        <div className="search-container">
+          <input type="text" placeholder="Search for any IP address or domain" name="search"/>
+          <button type="submit"><FaAngleRight /></button>
+        </div>
+
+        <div className="display-info">
+          <div className="sub">
+            <h4>Ip Address</h4>
+            <p>192.34.34.2323.23</p>
+          </div>
+          <div className="sub">   
+            <h4>Location</h4>
+            <p>Brooklyn, NY 10001</p></div>
+          <div className="sub">
+            <h4>Time Zone</h4>
+            <p>UTC -05:00</p>
+          </div>
+          <div className="sub">
+            <h4>ISP</h4>
+            <p>SpaceX Starlink</p>
+          </div>
+        </div>
+
+      </div>
+
+
+      <div ref={mapRef} id="map"></div>
     </div>
   );
 }
